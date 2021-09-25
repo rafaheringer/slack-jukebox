0bac42d575c9a5133fcb84af615b6eaabb07bf66
Rafael Heringer <rafael@nibo.com.br>
2021-09-25 16:36:54 -0300

    Start the browser tabs async

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ import SpotifyService from './services/spotify.js';
     var browserInstance = await browser.startBrowser();
 
     // Slack Tab
-    var slackTab = new SlackService(browserInstance);
-    await slackTab.init();
-    await slackTab.doLogin();
-    await slackTab.joinHuddle();
-    // await slackTab.sendMessage(':headphones: Hey, I\'m here! Let\'s listen a good music... :notes:');
+    async function slack() {
+        var slackTab = new SlackService(browserInstance);
+        await slackTab.init();
+        await slackTab.doLogin();
+        await slackTab.joinHuddle();
+        // await slackTab.sendMessage(':headphones: Hey, I\'m here! Let\'s listen a good music... :notes:');
 
-    slackTab.neverMuteWatcher();
-    slackTab.acceptInvitationWatcher();
+        slackTab.neverMuteWatcher();
+        slackTab.acceptInvitationWatcher();
+    }
+    
 
     // Spotify tab
-    var spotifyTab = new SpotifyService(browserInstance);
-    await spotifyTab.init();
-    await spotifyTab.doLogin();
-    await spotifyTab.doSearchAndPlayFirstResult('top brasil');
+    async function spotify() {
+        var spotifyTab = new SpotifyService(browserInstance);
+        await spotifyTab.init();
+        await spotifyTab.doLogin();
+        await spotifyTab.doSearchAndPlayFirstResult('top brasil');
+    }
+
+    // START!
+    slack();
+    spotify();
 
 })();
